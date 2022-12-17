db7773a1c4fdf413b88838f50fab91f1ce0de97c
PrithviVK <116768992+PrithviVK@users.noreply.github.com>
2022-12-17 10:30:03 -0500

    Add files via upload

---
+++ HW07/test/src/edu/umb/cs680/hw07/FileSystemTest.java
@@ -0,0 +1,40 @@
+package edu.umb.cs680.hw07;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+
+
+class FileSystemTest {
+
+	private static LocalDateTime d1=LocalDateTime.now();
+	
+	private String[] dirToStringArray(Directory d) {
+		String[] dirInfo = {
+				null,d.getName(), Integer.toString(d.getSize()),d.getDateTime().toString()};
+						
+		return dirInfo;
+	}
+
+	@Test
+	public void fileEqualityTest() {
+		FileSystem obj1=FileSystem.getFileSystem();
+		FileSystem obj2=FileSystem.getFileSystem();
+		assertEquals(obj1,obj2);
+	}
+	
+	@Test
+	public void testRootdirectory() {
+		String[] expected= {null,"root","0",d1.toString()};
+		Directory root=new Directory(null,"root",0,d1);
+		FileSystem.getFileSystem().appendRootDir(root);
+		Directory actual_directory=FileSystem.getFileSystem().getRootDirs().get(0);
+		String[] actual=dirToStringArray(actual_directory);
+		assertArrayEquals(expected,actual);
+	}
+
+}
